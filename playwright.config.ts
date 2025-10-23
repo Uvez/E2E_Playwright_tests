dee5f6d78f30e2d75e0e9eb0acaad27aae975623
Uvez <Uvez1993@gmail.com>
2025-10-23 20:13:55 +0530

    Updated Config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || 'https://parabank.parasoft.com/',
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    //video: 'retain-on-failure'
   },
   projects: [
 
@@ -29,8 +28,5 @@ export default defineConfig({
      },
     { name: 'firefox',  use: { ...devices['Desktop Firefox'], storageState :'.auth/user.json' } },
     { name: 'webkit',   use: { ...devices['Desktop Safari'], storageState :'.auth/user.json'} },
-    // Mobile example
-    //{ name: 'mobile-chrome', use: { ...devices['Pixel 7'] } }
   ],
-  //grepInvert: process.env.SKIP_FLAKY ? /@flaky/ : undefined
 });
